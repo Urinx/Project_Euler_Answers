b26fb9311d79def717a0e7f6b99fe48611958c5e
Urinx <1336006643@qq.com>
2014-05-05 22:32:31 -0400

    answer to problem 9

---
+++ 009.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+'''
+
+Special Pythagorean triplet
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+'''
+
+'''
+
+a<b<c
+a^2+b^2=c^2
+a+b+c=1000   ==> a^2+b^2=(1000-a-b)^2  ==> a+b-ab/1000=500
+             ==> a<=333 a+b<=666
+
+a=m^2−n^2
+b=2mn
+c=m^2+n^2
+
+'''
+
+def anwser():
+	for m in xrange(30,10,-1):
+		for n in xrange(m-1,0,-1):
+			a=m**2-n**2
+			b=2*m*n
+			c=m**2+n**2
+			if a+b+c==1000:
+				print '%d %d %d' % (a,b,c)
+				print a*b*c
+
+import time
+tStart=time.time()
+anwser()
+print 'run time=',time.time()-tStart
+#375 200 425
+#31875000
