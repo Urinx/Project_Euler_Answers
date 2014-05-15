84fe754845d0c43e7e73049d0035b051b5a9f429
Urinx <1336006643@qq.com>
2014-05-14 22:14:41 -0400

    P016

---
+++ 016.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+"""
+
+Power digit sum
+
+2^15 = 32768 and the sum of its digits is 3+2+7+6+8=26.
+What is the sum of the digits of the number 2^1000?
+
+"""
+
+def anwser():
+    print sum([int(i) for i in list(str(2**1000))])
+
+import time
+tStart=time.time()
+anwser()
+print 'run time=',time.time()-tStart
+# 1366
