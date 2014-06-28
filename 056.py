a55648438f251c3db107af16ba6ef82276723af9
Urinx <1336006643@qq.com>
2014-06-28 20:34:31 +0800

    P056

---
+++ 056.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+"""
+
+Powerful digit sum
+
+A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+
+"""
+import itertools
+
+def anwser():
+    m=0
+    for i in itertools.permutations(range(1,100),2):
+    	t=reduce(lambda x,y:x+int(y),list(str(i[0]**i[1])),0)
+    	if t>m: m=t
+    print m
+
+import time
+tStart=time.time()
+anwser()
+print 'run time=',time.time()-tStart
+# 972
+# run time= 0.551580905914
