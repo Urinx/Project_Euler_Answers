483a418378359ea9632f320231b7ecc4b966111b
Urinx <1336006643@qq.com>
2014-11-22 10:06:06 +0800

    Pleace forgive me for my recent commit, and it would last for a month around because of the coming exam and something else

---
+++ 118.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+"""
+Pandigital prime sets
+
+Using all of the digits 1 through 9 and concatenating them freely to form decimal integers, different sets can be formed. Interestingly with the set {2,5,47,89,631}, all of the elements belonging to it are prime.
+How many distinct sets containing each of the digits one through nine exactly once contain only prime elements?
+
+"""
+def answer():
+	
+
+import time
+tStart=time.time()
+answer()
+print 'run time=',time.time()-tStart
+# 100808458960497
+# run time= 0.000127077102661
