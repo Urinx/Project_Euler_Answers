73398ccbff1c35f0aa5c20eccdf4f021f2063160
Urinx <1336006643@qq.com>
2014-12-19 22:51:41 +0800

    P130

---
+++ 130.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+"""
+Composites with prime repunit property
+
+A number consisting entirely of ones is called a repunit. We shall define R(k) to be a repunit of length k; for example, R(6) = 111111.
+Given that n is a positive integer and GCD(n, 10) = 1, it can be shown that there always exists a value, k, for which R(k) is divisible by n, and let A(n) be the least such value of k; for example, A(7) = 6 and A(41) = 5.
+You are given that for all primes, p > 5, that p − 1 is divisible by A(p). For example, when p = 41, A(41) = 5, and 40 is divisible by 5.
+However, there are rare composite values for which this is also true; the first five examples being 91, 259, 451, 481, and 703.
+Find the sum of the first twenty-five composite values of n for which
+GCD(n, 10) = 1 and n − 1 is divisible by A(n).
+
+"""
+
+from projecteuler import is_prime
+
+dnp = set()    # set of deceptive non-primes
+L = 25
+n = 91    # start with first valid n given in the problem description
+
+while len(dnp) < L:
+    if not is_prime(n) and pow(10, n-1, 9*n) == 1:
+        dnp.add(n)
+    n += 2
+
+print "Project Euler 130 Solution =", sum(dnp)
+# 149253
